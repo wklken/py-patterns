62c11ff79ed80be9468461e9f346d1e93c71eb24
wklken <wklken@yeah.net>
2015-08-24 22:38:16 +0800

    add composit and memento

---
+++ composite.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+组合模式
+
+将对象组合成树状结构以表示`部分-整体`的层次结构
+使得用户对单个对象和组合对象的使用具有一致性
+
+- 适用: 需求中体现部分与整体层次的结构, 希望用户可以忽略组合对象与单个对象的不同, 统一地使用组合结构中的所有对象时, 就应该考虑使用组合模式
+- 优点: 让用户可以一致性的使用组合结构和单个对象
+"""
+
+
+class Component(object):
+    """
+    组合中的对象声明接口
+
+    在适当情况下, 实现所有类共有接口的默认行为
+
+    声明接口用于访问和管理Component的子部件
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def add(self, component):
+        pass
+
+    def remove(self, component):
+        pass
+
+    def display(self, depth):
+        pass
+
+
+class Leaf(Component):
+    """
+    组合中表示叶节点对象, 叶节点没有子节点
+    """
+
+    def add(self, component):
+        print "can not add to a leaf"
+
+    def remove(self, component):
+        print "can not remove from a leaf"
+
+    def display(self, depth):
+        print '-' * depth + self.name
+
+
+class Composite(Component):
+    """
+    定义有枝节点行为, 用于存储子部件
+
+    实现相关操作
+    """
+
+    def __init__(self, name):
+        super(Composite, self).__init__(name)
+        self.__children = []
+
+    def add(self, component):
+        self.__children.append(component)
+
+    def remove(self, component):
+        self.__children.remove(component)
+
+    def display(self, depth):
+        print '-' * depth + self.name
+        for c in self.__children:
+            c.display(depth + 2)
+
+
+if __name__ == '__main__':
+    root = Composite("root")
+
+    root.add(Leaf("A"))
+    root.add(Leaf("B"))
+
+    comp = Composite("X")
+    comp.add(Leaf("XA"))
+    comp.add(Leaf("XB"))
+
+    root.add(comp)
+
+    root.display(1)
+
---
+++ memento.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+备忘录
+
+在不破坏封装性的前提下, 捕获一个对象的内部状态, 并在该对象之外保存这个状态
+
+这样以后可以将该对象恢复到原先保存的状态
+
+- 将要保存的细节封装到memento中
+- 比较适合功能比较复杂, 需要维护或记录属性的历史类, 或者需要保存部分属性
+- 例如: 保存历史命令,  并执行撤销操作
+- 备忘录可以把复杂的对象内部信息对其他对象屏蔽起来
+"""
+
+
+class Originator(object):
+    """
+    创建一个备忘录Memento, 用以记录当前时刻的内部状态, 并可以使用备忘录恢复内部状态
+    """
+
+    def __init__(self, state):
+        self.__state = state
+
+    @property
+    def state(self):
+        return self.__state
+
+    @state.setter
+    def state(self, value):
+        self.__state = value
+
+    def create_memento(self):
+        return Memento(self.__state)
+
+    def set_memento(self, memento):
+        self.__state = memento.state
+
+    def show(self):
+        print "State:", self.__state
+
+
+class Memento(object):
+    """
+    负责存储Originator的内部状态, 并可以防止除Originator以外的其他对象访问
+    """
+    def __init__(self, state):
+        self.__state = state
+
+    @property
+    def state(self):
+        return self.__state
+
+
+class Caretaker(object):
+    """
+    负责保存好备忘录Memento
+    """
+    def __init__(self):
+        self.__memento = None
+
+    @property
+    def memento(self):
+        return self.__memento
+
+    @memento.setter
+    def memento(self, value):
+        self.__memento = value
+
+
+if __name__ == '__main__':
+    o = Originator("init")
+    o.show()
+    o.state = "begin"
+    o.show()
+
+    mem = o.create_memento()
+
+    # save it
+    c = Caretaker()
+    c.memento = mem
+
+    o.state = "change"
+    o.show()
+
+    # recover
+    o.set_memento(c.memento)
+    o.show()
